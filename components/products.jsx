389d25fe03d5b53c73ac2dd8337618b290297d13
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-07-22 11:51:59 +0300

    added pagination

--- components/products.jsx
+++ components/products.jsx
@@ -8,6 +8,8 @@ import Search from "./search";
 const Products = () => {
   const [data, setData] = useState([]);
   const [dataItems, setDataItems] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
 
   const skeletonData = [
     {
@@ -37,20 +39,33 @@ const Products = () => {
         .catch((error) => console.log(error));
 
       setData(x);
-
     };
 
     setTimeout(fetchdata, 2000);
   }, []);
 
   function handleSearch(term) {
-    const items = data.products?.filter((item) => 
-      item.title.toLowerCase().includes(term.toLowerCase()) || 
-      item.description.toLowerCase().includes(term.toLowerCase()));
-    
-    console.log(items);
-  
+    const items = data.products?.filter(
+      (item) =>
+        item.title.toLowerCase().includes(term.toLowerCase()) ||
+        item.description.toLowerCase().includes(term.toLowerCase())
+    );
+
     setDataItems(items);
+  }
+
+  // Calculate the items to display for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data?.products?.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(data?.products?.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
   }
 
   return (
@@ -72,28 +87,36 @@ const Products = () => {
         {dataItems ? (
           <div className=" mx-7 ">
             <div>
-            {dataItems?.length == 0 ? <div className="w-full flex justify-center align-middle text-center">No compatible items</div> : <div></div>}
+              {dataItems?.length == 0 ? (
+                <div className="w-full flex justify-center align-middle text-center">
+                  No compatible items
+                </div>
+              ) : (
+                <div></div>
+              )}
 
-            <div className="grid grid-cols-3">
-            {dataItems?.map((item) => (
-              <div
-                key={item.id}
-                className="bg-white m-1 rounded-sm p-2 hover:bg-gray-100 transition-shadow cursor-pointer"
-              >
-                <div className="font-semibold text-lg">{item.title}</div>
-                <div className="text-sm line-clamp-3">{item.description}</div>
-                <div className="font-semibold text-sm">{item.category}</div>
-                <div className="font-semibold text-sm">${item.price}</div>
-                <div>{item.brand}</div>
-                <div>{item.rating}</div>
+              <div className="grid grid-cols-3">
+                {dataItems?.map((item) => (
+                  <div
+                    key={item.id}
+                    className="bg-white m-1 rounded-sm p-2 hover:bg-gray-100 transition-shadow cursor-pointer"
+                  >
+                    <div className="font-semibold text-lg">{item.title}</div>
+                    <div className="text-sm line-clamp-3">
+                      {item.description}
+                    </div>
+                    <div className="font-semibold text-sm">{item.category}</div>
+                    <div className="font-semibold text-sm">${item.price}</div>
+                    <div>{item.brand}</div>
+                    <div>{item.rating}</div>
+                  </div>
+                ))}
               </div>
-            ))}
-            </div>
             </div>
           </div>
         ) : (
           <div>
-            {!data.products ? (
+            {!currentItems ? (
               <div className="bg-slate-100 w-full">
                 <div className="grid grid-cols-3 mx-7">
                   {skeletonData.map((item) => (
@@ -128,7 +151,7 @@ const Products = () => {
               </div>
             ) : (
               <div className="grid grid-cols-3 mx-7">
-                {data.products?.map((item) => (
+                {currentItems.map((item) => (
                   <div
                     key={item.id}
                     className="bg-white m-1 rounded-sm p-2 hover:bg-gray-100 transition-shadow cursor-pointer"
@@ -147,6 +170,23 @@ const Products = () => {
             )}
           </div>
         )}
+
+        <div>
+          <nav>
+            <ul className="flex w-full h-auto justify-center">
+              {pageNumbers.map((number) => (
+                <li key={number} className="flex">
+                  <a
+                    onClick={() => paginate(number)}
+                    className="bg-blue-400 mr-2 mt-10 mb-28 px-5 py-3 text-white text-center rounded-sm hover:bg-slate-700 cursor-pointer"
+                  >
+                    {number}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
