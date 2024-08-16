8482c8ce8727fda73211a639b90c1d72ab9b653e
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-08-16 11:49:02 +0300

    added comments and modified the skeleton loader

--- app/page.jsx
+++ app/page.jsx
@@ -6,13 +6,17 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-slate-100">
       <section className="">
-        <Image
+        {/* <Image
           src="/home-heroe.png"
           alt="home-heroe-image"
           className="w-full h-2/3 max-h-96 pb-2"
           width={1920}
           height={1080}
-        />
+        /> */}
+        <div className="min-h-96 flex justify-center items-center flex-col pt-48 pb-28">
+          <h1 className="font-bold text-6xl ">FREEZEDY EPZ</h1>
+          <h1 className="font-semibold text-4xl pt-5 text-slate-800">FROM SEED TO FORK</h1>
+        </div>
       </section>
         <Products />
       <section></section>
--- components/products.jsx
+++ components/products.jsx
@@ -6,11 +6,12 @@ import "react-loading-skeleton/dist/skeleton.css";
 import Search from "./search";
 
 const Products = () => {
-  const [data, setData] = useState([]);
-  const [dataItems, setDataItems] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(5);
+  const [data, setData] = useState([]); //Data from json API
+  const [dataItems, setDataItems] = useState(null); //Search data
+  const [currentPage, setCurrentPage] = useState(1); //State for our current page
+  const [itemsPerPage] = useState(5); // State for items per page
 
+  //Mock Data for skeleton loader
   const skeletonData = [
     {
       id: 1,
@@ -33,17 +34,18 @@ const Products = () => {
   ];
 
   useEffect(() => {
-    const fetchdata = async () => {
-      const x = await fetch("https://dummyjson.com/products")
+    const fetchData = async () => {
+      const dummyData = await fetch("https://dummyjson.com/products")
         .then((res) => res.json())
         .catch((error) => console.log(error));
 
-      setData(x);
+      setData(dummyData);
     };
 
-    setTimeout(fetchdata, 2000);
+    setTimeout(fetchData, 1000);
   }, []);
 
+  // Search functionality
   function handleSearch(term) {
     const items = data.products?.filter(
       (item) =>
@@ -54,6 +56,7 @@ const Products = () => {
     setDataItems(items);
   }
 
+  //implementing pagination
   // Calculate the items to display for the current page
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -115,20 +118,19 @@ const Products = () => {
             </div>
           </div>
         ) : (
-          <div>
+          <div className="max-w-screen box-border">
             {!currentItems ? (
-              <div className="bg-slate-100 w-full">
-                <div className="grid grid-cols-3 mx-7">
+                <div className="grid grid-cols-3 mx-7 max-sm:grid-cols-1  box-border w-[1000px] max-sm:w-auto max-lg:w-[600px]">
                   {skeletonData.map((item) => (
                     <div
                       key={item.id}
                       className="bg-white m-1 rounded-sm p-2 hover:bg-gray-100 transition-shadow cursor-pointer"
                     >
                       {/* <div className="font-semibold text-lg ">
-                    <Skeleton count={1} height={100} width={100} circle />
-                  </div> */}
-                      <div className="font-semibold text-lg ">
-                        <Skeleton count={1} height={40} />
+                        <Skeleton count={1} height={100} width={100} circle />
+                      </div> */}
+                      <div className="font-semibold text-lg">
+                        <Skeleton count={1} height={40}/>
                       </div>
                       <div className="text-sm line-clamp-3 ">
                         <Skeleton count={1} height={20} />
@@ -148,13 +150,12 @@ const Products = () => {
                     </div>
                   ))}
                 </div>
-              </div>
             ) : (
-              <div className="grid grid-cols-3 mx-7">
+              <div className="grid grid-cols-3 mx-7 max-sm:grid-cols-1 w-auto box-border">
                 {currentItems.map((item) => (
                   <div
                     key={item.id}
-                    className="bg-white m-1 rounded-sm p-2 hover:bg-gray-100 transition-shadow cursor-pointer"
+                    className="bg-white m-1 rounded-sm p-2 hover:bg-gray-100 transition-shadow cursor-pointer w-auto box-border"
                   >
                     <div className="font-semibold text-lg">{item.title}</div>
                     <div className="text-sm line-clamp-3">
@@ -171,21 +172,19 @@ const Products = () => {
           </div>
         )}
 
-        <div>
-          <nav>
-            <ul className="flex w-full h-auto justify-center">
+        <div className="w-auto">
+            <ul className="flex w-auto h-auto justify-center box-border">
               {pageNumbers.map((number) => (
                 <li key={number} className="flex">
                   <a
                     onClick={() => paginate(number)}
-                    className="bg-blue-400 mr-2 mt-10 mb-28 px-5 py-3 text-white text-center rounded-sm hover:bg-slate-700 cursor-pointer"
+                    className="bg-blue-400 mr-2 mt-10 mb-28 px-5 py-3 text-white text-center rounded-sm hover:bg-slate-700 cursor-pointer box-border"
                   >
                     {number}
                   </a>
                 </li>
               ))}
             </ul>
-          </nav>
         </div>
       </div>
     </div>
